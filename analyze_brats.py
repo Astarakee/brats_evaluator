8e3e39fea161f259495b20dc5c8113af34d34028
Mehdi Astaraki <astarakee@gmail.com>
2025-07-07 15:42:18 +0200

    clean scripts

--- analyze_brats.py
+++ analyze_brats.py
@@ -56,24 +56,6 @@ def analyze_exam(
     pred_masks = read_nifti(prediction_path)
     pred_masks = pred_masks.astype(int)
 
-    # manually set BraTS config; uncomment for manual configuration
-    # evaluator = Panoptica_Evaluator(
-    #     expected_input=InputType.SEMANTIC,
-    #     instance_approximator=ConnectedComponentsInstanceApproximator(),
-    #     instance_matcher=NaiveThresholdMatching(),
-    #     segmentation_class_groups=SegmentationClassGroups(
-    #         {
-    #             "ET": (3, False),
-    #             "NET": (1, False),
-    #             "ED": (2, False),
-    #             "TC": LabelMergeGroup([1, 3], False),
-    #             "WT": LabelMergeGroup([1, 2, 3], False),
-    #         }
-    #     ),
-    # )
-
-    # evaluator.save_to_config(panoptica_config_path)
-
     evaluator = Panoptica_Evaluator.load_from_config(panoptica_config_path)
 
     # call evaluate
@@ -83,21 +65,19 @@ def analyze_exam(
         # subject_name=identifier,
     )
 
-    # for key, value in result.items():
-    #     print(key, value)
-
     # convert to dict and add identifier
     results = {k: r.to_dict() for k, r in group2result.items()}
     results["subject_name"] = identifier
+
     return results
 
 
-if __name__ == "__main__":
-    results = analyze_exam(
-        prediction_path="test_case/brats202x/zyx_2019.nii.gz",
-        label_path="test_case/brats202x/simple.nii.gz",
-        identifier="test_case",
-        panoptica_config_path="brats_config.yaml",
-    )
-
-    print(results)
+# if __name__ == "__main__":
+#     results = analyze_exam(
+#         prediction_path="./sample_data/preds/BraTS-MET-01352-000.nii.gz",
+#         label_path="./sample_data/reference/BraTS-MET-01352-000.nii.gz",
+#         identifier="BraTS-MET-01352-000.nii.gz",
+#         panoptica_config_path= "./brats-configs/config_mets.yaml",
+#     )
+#
+#     print(results)
